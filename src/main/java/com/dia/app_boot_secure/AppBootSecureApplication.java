dc7a7d3704ec81be7f7cfa413e65c66ba7d0b42c
Afiss dia <afissdia07@gmail.com>
2022-03-28 17:01:52 +0000

    my first commit

---
+++ src/main/java/com/dia/app_boot_secure/AppBootSecureApplication.java
@@ -0,0 +1,13 @@
+package com.dia.app_boot_secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBootSecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBootSecureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dia/app_boot_secure/AppBootSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dia.app_boot_secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBootSecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
