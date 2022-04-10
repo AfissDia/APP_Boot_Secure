9ee019a141ecea2618f00c97062675eb1e000c94
Afiss dia <afissdia07@gmail.com>
2022-04-10 03:18:53 +0000

    after update delete for country

--- src/main/java/com/dia/app_boot_secure/controller/CountryController.java
+++ src/main/java/com/dia/app_boot_secure/controller/CountryController.java
@@ -5,26 +5,40 @@ import com.dia.app_boot_secure.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CountryController {
-   // public class ApplicationController {
+    // public class ApplicationController {
     @Autowired
     private CountryService countryService;
-        @GetMapping("/countries")
-        public String goCountries(Model model){
-            List<Country> listCountries=countryService.getCountries();
-            model.addAttribute("countries",listCountries);
-            return "country";
+
+    @GetMapping("/countries")
+    public String goCountries(Model model) {
+        List<Country> listCountries = countryService.getCountries();
+        model.addAttribute("countries", listCountries);
+        return "country";
         //}
     }
+
     @PostMapping("/countries/addNew")
-    public String saveCountries(Country country){
-            countryService.save(country);
-            return "redirect:/countries";
+    public String saveCountries(Country country) {
+        countryService.save(country);
+        return "redirect:/countries";
+    }
+
+    @RequestMapping("countries/findById")
+    @ResponseBody
+    public Optional<Country> findById(int id) {
+        return countryService.findById(id);
+    }
+
+    @RequestMapping(value = "countries/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Country country) {
+        countryService.save(country);
+        return "redirect:/countries";
     }
 }
