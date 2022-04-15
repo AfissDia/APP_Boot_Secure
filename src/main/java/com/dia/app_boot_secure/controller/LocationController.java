4a5e8993d367df065aa69dc79c3954c0fc2d5503
Afiss dia <afissdia07@gmail.com>
2022-04-15 13:27:32 +0000

    comiit 15/04/2022

--- src/main/java/com/dia/app_boot_secure/controller/LocationController.java
+++ src/main/java/com/dia/app_boot_secure/controller/LocationController.java
@@ -1,12 +1,65 @@
 package com.dia.app_boot_secure.controller;
 
+import com.dia.app_boot_secure.model.Country;
+import com.dia.app_boot_secure.model.Location;
+import com.dia.app_boot_secure.model.State;
+import com.dia.app_boot_secure.service.CountryService;
+import com.dia.app_boot_secure.service.LocationService;
+import com.dia.app_boot_secure.service.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LocationController {
+    @Autowired
+    private StateService stateService;
+    @Autowired
+    private CountryService countryService;
+    @Autowired
+    private LocationService locationService;
+
+    // Get All State
     @GetMapping("/locations")
-    public String goLocation() {
+    public String goStates(Model model) {
+        List<State> listState = stateService.getStates();
+        model.addAttribute("states", listState);
+
+        List<Country> listCountry = countryService.getCountries();
+        model.addAttribute("countries", listCountry);
+
+        List<Location> listLocation=locationService.getLocation();
+        model.addAttribute("location",listLocation);
+
         return "location";
+        //}
+    }
+
+    @PostMapping("/locations/addNew")
+    public String savelocation(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping("locations/findById")
+    @ResponseBody
+    public Optional<Location> findById(int id) {
+        return locationService.findById(id);
+    }
+
+    @RequestMapping(value = "locations/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value = "locations/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        locationService.delete(id);
+        return "redirect:/locations";
     }
 }
